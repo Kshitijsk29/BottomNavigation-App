7d24cfa1f378a54e2617a560767b8fcd9f409e45
kshitij <albatros.29kr@gmail.com>
2024-06-26 19:35:18 +0530

    initial commit

---
+++ app/src/main/java/com/nextin/bottomnavigationapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.nextin.bottomnavigationapp
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        hideSystemUI()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.navBottom)
+
+        replaceFrameWithFragment(HomeFragment())
+
+        bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.itemHome -> replaceFrameWithFragment(HomeFragment())
+                R.id.itemSearch-> replaceFrameWithFragment(SearchFragment())
+                R.id.itemFeed -> replaceFrameWithFragment(FeedFragment())
+                R.id.itemProfile -> replaceFrameWithFragment(ProfileFragment())
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFrameWithFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window,
+            window.decorView.findViewById(android.R.id.content)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+
+            // When the screen is swiped up at the bottom
+            // of the application, the navigationBar shall
+            // appear for some time
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
